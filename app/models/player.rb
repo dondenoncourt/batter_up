ef32031be0c174beaad977d457b96ca59fc1671c
Don Denoncourt <dondenoncourt@gmail.com>
2014-07-08 15:37:12 -0400

    annotate

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,16 +2,10 @@
 #
 # Table name: players
 #
-#  player_id      :string(255)      primary key
-#  birth_year     :integer
-#  first          :string(255)
-#  last           :string(255)
-#  at_bats        :integer
-#  hits           :integer
-#  doubles        :integer
-#  triples        :integer
-#  home_runs      :integer
-#  runs_batted_in :integer
+#  player_id  :string(255)      primary key
+#  birth_year :integer
+#  first      :string(255)
+#  last       :string(255)
 #
 
 class Player < ActiveRecord::Base
